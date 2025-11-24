a9a0e8d69df4dd686b74ade2593e8f7337a44d82
nedtonks <104468000@student.swin.edu.au>
2025-11-24 12:24:26 +1100

    Clean repository: removed DerivedData and Xcode user data

---
+++ StockPortfolio/StockPortfolio/StockPortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockPortfolioApp.swift
+//  StockPortfolio
+//
+//  Created by Ned Tonks on 24/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct StockPortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
